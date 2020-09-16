e0492c7a69d35c656c9ef14c8da562dab9f5b899
Edson <70923817+Skaylove@users.noreply.github.com>
2020-09-15 23:12:50 -0700

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,4 +1,11 @@
 h = int(input('introduzca las horas  '))
 m = int(input('introduzca los minutos   '))
 s = int(input('introduzca los segundos  '))
-print('horas {} - minutos {} - segundos {}'.format(h,m,s+1))
+if(s>=0 and s<=58):
+    print('horas {} - minutos {} - segundos {}'.format(h,m,s+1))
+elif(s==59) and  (m == 59):
+    print('horas {} - minutos {} - segundos {}'.format(h+1,0,0))
+elif(s == 59):
+    print('horas {} - minutos {} - segundos {}'.format(h,m+1,0))
+else:
+    print('Error en la entradad de datos')
